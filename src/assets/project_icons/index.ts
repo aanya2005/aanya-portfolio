d8fdacd9c40945c4b102d3ae3d2e80ffd8272676
aanyaa2@uci.edu <aanyaa2@uci.edu>
2026-07-19 11:39:18 -0700

    Initial portfolio website

---
+++ src/assets/project_icons/index.ts
@@ -0,0 +1,8 @@
+// Project icons module - add your project icon imports here
+// Example:
+// import MyProjectIcon from './MyProjectIcon.webp';
+//
+// export { MyProjectIcon };
+// export default { MyProjectIcon };
+export {};
+export default {};
